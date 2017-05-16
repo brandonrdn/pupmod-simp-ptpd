e99002e6d955674fa2b232f88450eae42a0c3151
Brandon Riden <brandon.riden@onyxpoint.com>
2017-05-16 13:30:09 -0400

    latest

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,8 @@
 require 'beaker-rspec'
 require 'tmpdir'
 require 'yaml'
-#require 'simp/beaker_helpers'
-#include Simp::BeakerHelpers
+require 'simp/beaker_helpers'
+include Simp::BeakerHelpers
 
 unless ENV['BEAKER_provision'] == 'no'
   hosts.each do |host|
@@ -18,36 +18,35 @@ end
 
 RSpec.configure do |c|
   # ensure that environment OS is ready on each host
-#  fix_errata_on hosts
+  fix_errata_on hosts
 
   # Readable test descriptions
   c.formatter = :documentation
 
-  # # Configure all nodes in nodeset
+ # Configure all nodes in nodeset
   c.before :suite do
     begin
       # Install modules and dependencies from spec/fixtures/modules
-#      copy_fixture_modules_to( hosts )
+      copy_fixture_modules_to( hosts )
       begin
-        server = only_host_with_role(hosts, 'user')
-        server = only_host_with_role(hosts, 'default')
-      rescue ArgumentError =>e
+        servers = hosts_with_role(hosts, 'server')
+#      rescue ArgumentError =>e
       end
 
-#      # Generate and install PKI certificates on each SUT
-#      Dir.mktmpdir do |cert_dir|
-#        run_fake_pki_ca_on(server, hosts, cert_dir )
-#        hosts.each{ |sut| copy_pki_to( sut, cert_dir, '/etc/pki/simp-testing' )}
-#      end
+      # Generate and install PKI certificates on each SUT
+      # Dir.mktmpdir do |cert_dir|
+      #   run_fake_pki_ca_on(server, hosts, cert_dir )
+      #   hosts.each{ |sut| copy_pki_to( sut, cert_dir, '/etc/pki/simp-testing' )}
+      # end
 
       # add PKI keys
-#      copy_keydist_to(server)
-#    rescue StandardError, ScriptError => e
-#      if ENV['PRY']
-#        require 'pry'; binding.pry
-#      else
+      # copy_keydist_to(server)
+    # rescue StandardError, ScriptError => e
+      # if ENV['PRY']
+      #   require 'pry'; binding.pry
+      else
 #        raise e
       end
     end
   end
-#end
+# end
